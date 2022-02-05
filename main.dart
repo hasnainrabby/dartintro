de42317627b98bf4126f823183b01c75d2aef67d
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-02-05 14:36:13 +0600

    Initial commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+
+main()
+{
+  var country ;
+  String name;
+  int age;
+  country = 'Bangladesh';
+  name = "MD HASNAIN RABBY";
+  age = 25;
+  print(name);
+  print(age);
+  print(country);
+
+
+}
