288f146eda54c22c5db80c6f01c609abf7707aad
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-02-05 15:14:59 +0600

    Added Dart Type(Types of Variable) String,Number(Integer & Double)

--- main.dart
+++ main.dart
@@ -4,12 +4,13 @@ main()
   var country ;
   String name;
   int age;
+  double weight;
   country = 'Bangladesh';
   name = "MD HASNAIN RABBY";
+  weight = 73.56;
   age = 25;
   print(name);
   print(age);
+  print(weight);
   print(country);
-
-
 }
