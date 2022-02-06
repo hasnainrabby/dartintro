84f3254cda4da2dc2bd7565f7ef82fa8a1d8ff6f
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-02-06 12:38:30 +0600

    Added Boolean & Concatenation

--- main.dart
+++ main.dart
@@ -1,16 +1,24 @@
-
 main()
 {
+  //Types of Variable.
   var country ;
   String name;
-  int age;
-  double weight;
+  int age;                                                                                //number
+  double weight;                                                                 //number
+
+  bool  isTrue = true;                                                         //Boolean
+  bool  isFalse = false;                                                     //Boolean
+
+
+
+
   country = 'Bangladesh';
-  name = "MD HASNAIN RABBY";
+  name = "MD Hasnain Rabby";
   weight = 73.56;
   age = 25;
-  print(name);
-  print(age);
-  print(weight);
-  print(country);
+
+
+  print("${name.toUpperCase()} comes from $country. He is $age years old. His weights is $weight KG.");  //Concatenation - joining strings.
+  print(isTrue);
+  print(isFalse);
 }
