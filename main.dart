9f55fb9acf9b58ed41021f4cbdf9841d40f5a3fc
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-02-07 15:03:52 +0600

    Added Operator Addition,Subtraction,Multiplication,Division,Remainder.

--- main.dart
+++ main.dart
@@ -9,16 +9,28 @@ main()
   bool  isTrue = true;                                                         //Boolean
   bool  isFalse = false;                                                     //Boolean
 
-
-
-
   country = 'Bangladesh';
   name = "MD Hasnain Rabby";
   weight = 73.56;
   age = 25;
 
+  int a = 25;
+  int b = 100;
+  double  c = 10.5;
+
+ var Additon = a + b;                                                   //Operators +,-,*,/,%
+ var sub = b - a;
+ var multiplication = a * c;
+ var division = b/a;
+ var remainder = b%c;
+
+
+
 
   print("${name.toUpperCase()} comes from $country. He is $age years old. His weights is $weight KG.");  //Concatenation - joining strings.
+
   print(isTrue);
   print(isFalse);
+
+  print("$Additon,$sub,$multiplication,$division,$remainder");
 }
